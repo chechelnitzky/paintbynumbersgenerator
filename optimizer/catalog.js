f164d532224594660ec3efa789f9c2075f83b93f
chechelnitzky <chechelnitzky@gmail.com>
2026-08-20 14:01:52 -0400

    Add current product and marker recipe catalog

---
+++ optimizer/catalog.js
@@ -0,0 +1,44 @@
+window.PBN_CATALOG = {
+  "version": "2026-08-20",
+  "source": "paletas para optimizacion.rar",
+  "products": [
+    {
+      "id": "desierto-florido",
+      "name": "Desierto Florido",
+      "recipes": [
+        {"id":"desierto-florido-a","name":"A","source":"desierto florido.pdf","markers":["5","10","13","31","34","36","91","95","96","99","100","122","131","172","CG9","WG3"]},
+        {"id":"desierto-florido-b","name":"B","source":"desiertoo.pdf","markers":["5","10","13","31","32","34","91","95","96","99","100","122","131","172","WG2","WG8"]}
+      ]
+    },
+    {"id":"labios","name":"Labios","recipes":[{"id":"labios-a","name":"Actual","source":"Paint by number generator.pdf","markers":["2","5","7","9","10","26","35","45","66","70","96","122","126","196","BG3","WG8"]}]},
+    {"id":"limonero","name":"Limonero","recipes":[{"id":"limonero-a","name":"Actual","source":"limonero nuevo.pdf","markers":["25","31","41","43","45","54","66","95","96","102","133","142","144","BG9","CG5","CG9"]}]},
+    {"id":"tulipanes","name":"Tulipanes","recipes":[
+      {"id":"tulipanes-a","name":"A","source":"Paint by number generator referencia-tulipam.pdf","markers":["3","7","10","14","32","50","59","61","63","71","86","89","98","124","138","139"]},
+      {"id":"tulipanes-b","name":"B","source":"Paint by number generator Tulipan.pdf","markers":["5","8","10","14","36","48","50","59","61","71","76","86","88","107","120","138"]}
+    ]},
+    {"id":"lavanda","name":"Lavanda","recipes":[{"id":"lavanda-a","name":"Actual","source":"Paint by number generator lavanda.pdf","markers":["1","2","3","5","7","14","26","81","86","87","91","96","122","133","WG8","WG9"]}]},
+    {"id":"pareja-bici","name":"Pareja Bici","recipes":[{"id":"pareja-bici-a","name":"Actual","source":"pareja bici.pdf","markers":["7","14","28","58","70","73","74","75","82","95","96","121","135","144","WG2","WG8"]}]},
+    {"id":"moai","name":"Moai","recipes":[{"id":"moai-a","name":"Actual","source":"Paint by number generator MOAI.pdf","markers":["41","45","52","70","72","74","76","135","144","CG0.5","CG5","CG6","CG8","CG9","GG7","WG9"]}]},
+    {"id":"fondo-casa","name":"Fondo Casa","recipes":[{"id":"fondo-casa-a","name":"Actual","source":"Paint by number generator Fondo Casa.pdf","markers":["6","35","36","53","55","62","63","64","73","87","BG3","CG3","CG6","CG8","WG0.5","WG9"]}]},
+    {"id":"perrito-flor","name":"Perrito Flor","recipes":[{"id":"perrito-flor-a","name":"Actual","source":"perritoflor.pdf","markers":["1","3","5","7","13","18","29","31","32","81","82","89","95","100","145"]}]},
+    {"id":"arturo-prat","name":"Arturo Prat","recipes":[{"id":"arturo-prat-a","name":"Actual","source":"paleta arutro prat.pdf","markers":["1","29","32","95","99","100","101","102","107","120","173","BG7","BG9","CG9","GG5","GG7","WG8","WG9"]}]},
+    {"id":"girasol","name":"Girasol","recipes":[{"id":"girasol-a","name":"Actual","source":"girasol nuevo.pdf","markers":["24","31","49","62","70","76","92","95","99","100","141","CG6","CG7","CG9","GG1","WG8"]}]},
+    {"id":"muelle-almas","name":"Muelle Almas","recipes":[{"id":"muelle-almas-a","name":"Actual","source":"muelle almas.pdf","markers":["24","32","41","45","48","50","52","76","95","100","134","171","174","183","CG9","WG9"]}]},
+    {"id":"santiago","name":"Santiago","recipes":[{"id":"santiago-a","name":"Actual","source":"paleta stgo new.pdf","markers":["2","10","24","29","31","70","71","74","76","100","102","109","BG9","CG5","CG9","GG7"]}]},
+    {"id":"capri","name":"Capri","recipes":[
+      {"id":"capri-a","name":"A","source":"Paint by number generator Capria.pdf","markers":["5","36","48","56","57","71","74","76","91","100","107","139","144","BG9","CG0.5","GG5"]},
+      {"id":"capri-b","name":"B","source":"Paint by number generator capri.pdf","markers":["11","22","48","56","57","67","71","74","91","100","134","139","144","CG0.5","CG7","GG5"]}
+    ]},
+    {"id":"leoncito","name":"Leoncito","recipes":[{"id":"leoncito-a","name":"Actual","source":"Paint by number generator leoncito.pdf","markers":["32","33","47","53","56","68","91","96","131","BG3"]}]},
+    {"id":"torres-del-paine-o","name":"Torres del Paine O","recipes":[{"id":"torres-del-paine-o-a","name":"Actual","source":"Paint by number generator TDPo.pdf","markers":["11","14","57","98","100","104","144","BG1","BG9","CG5","CG6","CG7","CG9","GG5","WG5","WG9"]}]},
+    {"id":"mariposa","name":"Mariposa","recipes":[{"id":"mariposa-a","name":"Actual","source":"Paint by number generator Mariposa.pdf","markers":["4","22","32","56","59","71","74","81","82","83","84","86","97","107","137","143"]}]},
+    {"id":"amsterdam","name":"Amsterdam","recipes":[{"id":"amsterdam-a","name":"Actual","source":"Paint by number Amsterdam.pdf","markers":["22","24","26","36","49","52","61","96","131","173","178","182","BG9","GG5","GG7"]}]},
+    {"id":"florero","name":"Florero","recipes":[{"id":"florero-a","name":"Actual","source":"Paint by number Florero.pdf","markers":["4","10","27","29","36","53","91","96","120","122","174","175","BG7","CG9","WG5","WG7"]}]},
+    {"id":"montana","name":"Montaña","recipes":[{"id":"montana-a","name":"Actual","source":"Paleta montaña.pdf","markers":["2","9","24","34","35","41","50","61","66","76","120","172","BG5","BG9","CG0.5","CG5"]}]},
+    {"id":"firenze","name":"Firenze","recipes":[{"id":"firenze-a","name":"Actual","source":"Paint by number Firenze.pdf","markers":["1","2","22","36","56","64","65","76","91","107","133","142","173","BG9","CG9","GG7"]}]},
+    {"id":"princesa","name":"Princesa","recipes":[{"id":"princesa-a","name":"Actual","source":"PRINCESA PALETA NUEVA.pdf","markers":["12","25","33","45","56","58","59","66","86","88","182","CG1"]}]},
+    {"id":"casa-rio","name":"Casa Río","recipes":[{"id":"casa-rio-a","name":"Actual","source":"casa rio.pdf","markers":["1","3","11","23","48","52","56","58","64","65","70","73","87","92","123","166"]}]},
+    {"id":"vaca","name":"Vaca","recipes":[{"id":"vaca-a","name":"Actual","source":"Paint by number VACA.pdf","markers":["11","18","56","59","67","124","WG0.5","WG1","WG7","WG8"]}]},
+    {"id":"torres-del-paine","name":"Torres del Paine","recipes":[{"id":"torres-del-paine-a","name":"Actual","source":"Paint by number generator TDP.pdf","markers":["24","26","34","36","61","63","92","100","185","BG7","BG9","CG0.5","CG8","CG9","GG5","GG7"]}]}
+  ]
+};
